a8540e7ff8032f52d5b4a536c6cc21faca742ea8
James <jmcelroy@plexustech.net>
2020-05-19 14:08:24 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+oatmeal = Recipe.create(title: "Oatmeal")
+Ingredient.create(name: "Oats", quantity: "alot", recipe_id: oatmeal.id)
